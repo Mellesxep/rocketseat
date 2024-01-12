ab15b365496e2c656dc1c1f05a4473b63c4a1a34
Mellesxep <jorge55ausguto@gmail.com>
2024-01-11 23:56:21 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("ligth")
+
+  const img = document.querySelector("#profile img")
+
+  if(html.classList.contains("ligth")) {
+
+  img.setAttribute("src", "./assets/lightmodephoto.png")
+
+  } else {
+
+  img.setAttribute("src", "./assets/profile-dark.png")
+}
+
+}
